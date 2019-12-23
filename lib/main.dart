3c2865405879a0dff0f187adab4192a6323dbd3c
王绍森 <wangshaosen666@gmail.com>
2019-12-23 23:54:00 +0800

    form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,6 +8,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "您好呀，弗拉特尔!",
       home: MyRollCall(),
+      routes: <String, WidgetBuilder>{
+        "/home": (BuildContext context) => HomePage(),
+        "/about": (BuildContext context) => AboutPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Container(
+          margin: new EdgeInsets.only(top: 200, left: 40, right: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: "用户名"),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "密码"),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                  padding: EdgeInsets.only(left: 100, right: 100),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text(
+                    "登录",
+                  ),
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        title: Text("About"),
+      ),
     );
   }
 }
@@ -18,44 +72,52 @@ class _MyRollCallState extends State<MyRollCall> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("我的点名"),
-      ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 10, bottom: 10),
-        padding: const EdgeInsets.only(top: 10, bottom: 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
+        appBar: AppBar(
+          title: Text("我的点名"),
+          actions: <Widget>[
+            FlatButton(
+              child: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, "/home");
+              },
+            )
+          ],
+        ),
+        body: Container(
+          margin: const EdgeInsets.only(top: 10, bottom: 10),
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.4),
               offset: Offset(0, 0),
               blurRadius: 5,
               spreadRadius: 5,
             )
-          ]
-        ),
-        child:       Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          DatePickerDiy("开始时间", (val) {
-            setState(() {
-              _beginTime = val.toLocal();
-            });
-          }),
-          Text("至", style: TextStyle(fontSize: 24),),
-          DatePickerDiy("结束时间", (val) {
-            setState(() {
-              _endTime = val.toLocal();
-            });
-          }),
-          Icon(Icons.calendar_today),
-        ],
-      ),
-      )
-      
-
-    );
+          ]),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              DatePickerDiy("开始时间", (val) {
+                setState(() {
+                  _beginTime = val.toLocal();
+                });
+              }),
+              Text(
+                "至",
+                style: TextStyle(fontSize: 24),
+              ),
+              DatePickerDiy("结束时间", (val) {
+                setState(() {
+                  _endTime = val.toLocal();
+                });
+              }),
+              Icon(Icons.calendar_today),
+            ],
+          ),
+        ));
   }
 }
 
@@ -103,83 +165,4 @@ class _DatePickerDiyState extends State<DatePickerDiy> {
       ),
     );
   }
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _biggerFontSize = const TextStyle(fontSize: 18.0);
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Startup Name Generator"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final Iterable<ListTile> titles = _saved.map((WordPair pair) {
-        return ListTile(title: Text(pair.asPascalCase, style: _biggerFontSize));
-      });
-
-      final List<Widget> divided =
-          ListTile.divideTiles(context: context, tiles: titles).toList();
-      return Scaffold(
-        appBar: AppBar(title: Text("Saved Words")),
-        body: ListView(
-          children: divided,
-        ),
-      );
-    }));
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFontSize,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  RandomWordsState createState() => RandomWordsState();
 }
